fcd87fb8471c437d010ab4aafa46c71cdb765e94
juanm04 <juanm04@outlook.com>
2026-09-03 19:32:11 -0600

    push example

--- sql_load/test.sql
+++ sql_load/test.sql
@@ -1,9 +1,18 @@
-SELECT *
-FROM job_postings_fact
-LIMIT 5
+WITH company_totals AS (
+    SELECT 
+    name,
+        count(*) total_postings
+    FROM company_dim
+    INNER JOIN job_postings_fact AS company_name ON company_name.company_id = company_dim.company_id
+    GROUP BY company_dim.company_id
+)
 
-SELECT *
-FROM company_dim
---INNER JOIN job_postings_fact AS company_name ON company_name.company_id = company_dim.company_id
+SELECT *, 
+CASE 
+    WHEN total_postings < 10 THEN 'Small'
+    WHEN total_postings >= 10 AND total_postings < 50 THEN 'Medium'
+    ELSE 'Large'
+End AS company_size
+FROM company_totals
 
 
